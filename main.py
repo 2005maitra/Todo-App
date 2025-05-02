9ce1efa9d4536bf1e9c7d2b11aa42734c8296a3c
2005maitra <143825608+2005maitra@users.noreply.github.com>
2025-05-02 16:12:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ while True:
     user_action = input("type add,show,edit, complete or exit:")
     user_action = user_action.strip()
 
-
     if user_action.startswith("add"):
-        todo = user_action[4:]
+        todo = user_action[4:].strip()
+        if not todo:
+            todo = input("What todo would you like to add? ").strip()
 
         todos = function.get_todos()
-
-        todos.append(todo+'\n')
-
-        write_todos(todos,"todos.txt")
+        todos.append(todo + '\n')
+        function.write_todos(todos)
+        print(f"Added: {todo}")
 
     elif user_action.startswith("show"):
 
@@ -39,7 +39,7 @@ while True:
             new_todo = input("enter new todo:")
             todos[number]= new_todo +'\n'
 
-            write_todos(todos)
+            function.write_todos(todos)
         except ValueError:
             print("your command is not valid.")
             continue
@@ -59,7 +59,7 @@ while True:
 
             message = f"Todo {todo_to_remove} was removed from list."
             print(message)
-        except IndexErrorError:
+        except IndexError:
             print("there is no item  with the number.")
             continue
 
@@ -70,3 +70,69 @@ while True:
 
 print("bye!")
 
+
+import function  
+
+now = time.strftime("%b %d, %Y %H:%M:%S")
+print("It is", now)
+
+while True:
+    user_action = input("Type add, show, edit, complete or exit: ")
+    user_action = user_action.strip()
+
+    if user_action.startswith("add"):
+        todo = user_action[4:]
+
+        todos = function.get_todos()
+        todos.append(todo + '\n')
+        function.write_todos(todos, "todos.txt")
+
+    elif user_action.startswith("show"):
+        todos = function.get_todos()
+
+        for index, item in enumerate(todos):
+            item = item.strip('\n')
+            row = f"{index + 1} -- {item}"
+            print(row)
+
+    elif user_action.startswith("edit"):
+        try:
+            number = int(user_action[5:])
+            todos = function.get_todos()
+
+            new_todo = input("Enter new todo: ")
+            todos[number - 1] = new_todo + '\n'
+
+            function.write_todos(todos, "todos.txt")
+        except ValueError:
+            print("Your command is not valid. Please enter a number after 'edit'.")
+            continue
+        except IndexError:
+            print("Invalid todo number.")
+            continue
+
+    elif user_action.startswith("complete"):
+        try:
+            number = int(user_action[9:])
+
+            todos = function.get_todos()
+            index = number - 1
+            todo_to_remove = todos[index].strip('\n')
+            todos.pop(index)
+
+            function.write_todos(todos, "todos.txt")
+            message = f"Todo '{todo_to_remove}' was removed from the list."
+            print(message)
+        except IndexError:
+            print("There is no item with that number.")
+            continue
+        except ValueError:
+            print("Invalid input. Please provide a number after 'complete'.")
+            continue
+
+    elif user_action.startswith("exit"):
+        break
+    else:
+        print("Command is not valid.")
+
+print("Bye!")
